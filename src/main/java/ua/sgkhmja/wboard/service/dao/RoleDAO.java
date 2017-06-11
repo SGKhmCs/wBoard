e88f673fc8b768179dfe6347db40a32dff4a7473
Tsisar <fhantom.tp@gmail.com>
2017-06-11 22:23:22 +0300

    fix

---
+++ src/main/java/ua/sgkhmja/wboard/service/dao/RoleDAO.java
@@ -0,0 +1,23 @@
+package ua.sgkhmja.wboard.service.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ua.sgkhmja.wboard.security.SecurityUtils;
+
+/**
+ * Created by pavel on 10.05.17.
+ */
+@Component
+public class UserDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDAO(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public long getUserIdByCurrentLogin(){
+        String sql = "select id from jhi_user where login ='" + SecurityUtils.getCurrentUserLogin() + "'";
+        return this.jdbcTemplate.queryForObject(sql, long.class);
+    }
+}

20939e25bf188b312356fd77a42c70e5fddb7178
Tsisar <fhantom.tp@gmail.com>
2017-06-11 22:24:05 +0300

    fix

--- src/main/java/ua/sgkhmja/wboard/service/dao/RoleDAO.java
+++ src/main/java/ua/sgkhmja/wboard/service/dao/RoleDAO.java
@@ -2,22 +2,43 @@ package ua.sgkhmja.wboard.service.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import ua.sgkhmja.wboard.domain.Board;
+import ua.sgkhmja.wboard.domain.User;
 import ua.sgkhmja.wboard.security.SecurityUtils;
 
 /**
  * Created by pavel on 10.05.17.
  */
 @Component
-public class UserDAO {
+public class RoleDAO {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDAO(JdbcTemplate jdbcTemplate){
+    public RoleDAO(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public long getUserIdByCurrentLogin(){
-        String sql = "select id from jhi_user where login ='" + SecurityUtils.getCurrentUserLogin() + "'";
+    public Long isReader(User user, Board board){
+        Long usesId = user.getId();
+        Long boardId = board.getId();
+
+        String sql = "select id from reader where board_user_id in (select id from board_user where user_id = " +
+            usesId + "and board_id = " + boardId +" )";
+
+        return this.jdbcTemplate.queryForObject(sql, long.class);
+    }
+
+    public Long isWriter(User user, Board board){
+
+        String sql = "select id from writer where reader_id = " + isReader(user, board);
+
+        return this.jdbcTemplate.queryForObject(sql, long.class);
+    }
+
+    public Long isAdmin(User user, Board board){
+
+        String sql = "select id from admin where writer_id = " + isWriter(user, board);
+
         return this.jdbcTemplate.queryForObject(sql, long.class);
     }
 }
--- src/main/java/ua/sgkhmja/wboard/service/mapper/BoardMapper.java
+++ src/main/java/ua/sgkhmja/wboard/service/mapper/BoardMapper.java
@@ -12,9 +12,9 @@ import org.mapstruct.*;
 public interface BoardMapper extends EntityMapper <BoardDTO, Board> {
     @Mapping(source = "owner.id", target = "ownerId")
     @Mapping(source = "owner.login", target = "ownerLogin")
-    BoardDTO toDto(Board board); 
+    BoardDTO toDto(Board board);
     @Mapping(source = "ownerId", target = "owner")
-    Board toEntity(BoardDTO boardDTO); 
+    Board toEntity(BoardDTO boardDTO);
     /**
      * generating the fromId for all mappers if the databaseType is sql, as the class has relationship to it might need it, instead of
      * creating a new attribute to know if the entity has any relationship from some other entity
@@ -22,7 +22,7 @@ public interface BoardMapper extends EntityMapper <BoardDTO, Board> {
      * @param id id of the entity
      * @return the entity instance
      */
-     
+
     default Board fromId(Long id) {
         if (id == null) {
             return null;
